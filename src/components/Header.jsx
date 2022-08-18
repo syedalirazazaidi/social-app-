2deb544914ce057908ca3a0b1b51c9cd598f41ed
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-08-18 13:54:08 +0500

    added front end UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const onLogout = () => {};
+  const user = null;
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
