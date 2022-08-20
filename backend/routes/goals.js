e35000bb6e7f36991e826269817a5fdf49354ab6
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-08-20 15:45:34 +0500

    Add back

--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -9,6 +9,6 @@ const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/").get(protect, getGoals).post(protect, createGoals);
-router.route("/:id").delete(protect, deleteGoals).put(protect, updateGoal);
+router.route("/:id").delete(protect, deleteGoals).patch(protect, updateGoal);
 
 module.exports = router;
