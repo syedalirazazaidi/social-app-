e939cf8b2e543b50b12bebcf64606c4562c7274f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-08-21 23:43:11 +0500

    Add update

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -20,33 +20,45 @@ const createGoals = asyncHandler(async (req, res) => {
 
   res.status(200).json(goal);
 });
-const updateGoal = asyncHandler(async (req, res) => {
-  //   res.status(200).json({ message: `Update goals ${req.params.id}` });
-  const goal = await Goal.findById(req.params.id);
+// const updateGoal = asyncHandler(async (req, res) => {
+//   const goal = await Goal.findById(req.params.id);
 
-  if (!goal) {
-    res.status(400);
-    throw new Error("Goal not found");
-  }
+//   if (!goal) {
+//     res.status(400);
+//     throw new Error("Goal not found");
+//   }
+//   // Check for user
+//   if (!req.user) {
+//     res.status(401);
+//     throw new Error("User not found");
+//   }
+//   // Make sure the logged in user matches the goal user
+//   if (goal.user.toString() !== req.user.id) {
+//     res.status(401);
+//     throw new Error("User not authorized");
+//   }
 
-  // Check for user
-  if (!req.user) {
-    res.status(401);
-    throw new Error("User not found");
-  }
+//   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+//     new: true,
+//   });
 
-  // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== req.user.id) {
-    res.status(401);
-    throw new Error("User not authorized");
-  }
+//   res.status(200).json(updatedGoal);
+// });
 
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+export const updateGoal = async (req, res) => {
+  const { id } = req.params;
+  // const { title, message, creator, tags } = req.body;
+  const { text } = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, _id: id };
+
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
   res.status(200).json(updatedGoal);
-});
+};
+
 const deleteGoals = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
@@ -66,12 +78,8 @@ const deleteGoals = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error("User not authorized");
   }
-
   await goal.remove();
-
   res.status(200).json({ id: req.params.id });
-
-  //   res.status(200).json({ message: `delete goals ${req.params.id}` });
 });
 
 module.exports = {
